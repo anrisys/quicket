6dc50dac95d698ba5776a41c520a34816e20c12c
Anris Y. Simorangkir <anris.y.simorangkir@gmail.com>
2025-08-23 16:49:01 +0700

    Feat: Introduce internal/router pkg and docker-compose.test.yml

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
-	"github.com/anrisys/quicket/internal/validation"
+	"github.com/anrisys/quicket/internal/router"
 	"github.com/anrisys/quicket/pkg/di"
-	"github.com/anrisys/quicket/pkg/middleware"
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
-	swaggerFiles "github.com/swaggo/files"     // swagger embed files
-	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
 // @title Quicket API
@@ -39,59 +32,10 @@ func main() {
         log.Fatalf("Failed to initialize app: %v", err)
     }
     
-    router := gin.New()
-
-	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-		    return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
-        param.ClientIP,
-        param.TimeStamp.Format(time.RFC1123),
-        param.Method,
-        param.Path,
-        param.Request.Proto,
-        param.StatusCode,
-        param.Latency,
-        param.Request.UserAgent(),
-        param.ErrorMessage,
-    )
-	}))
-
-    if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-        validation.RegisterCustomValidation(v)
-    }
-	
-	router.Use(middleware.ZerologLogger(), gin.Recovery(), middleware.ErrorHandler())
-
-    router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-    registerRoutes(router, app)
+    r := router.SetupRouter(app)
     
     addr := fmt.Sprintf(":%s", app.Config.Server.Port)
-
-	if err := router.Run(addr); err != nil {
+	if err := r.Run(addr); err != nil {
         log.Fatalf("Server failed :%v", err)
-    }
-}
-
-func registerRoutes(r *gin.Engine, app *di.App)  {
-    // r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-    
-    public := r.Group("/api/v1")
-    {
-        public.POST("/register", app.UserHandler.Register)
-        public.POST("/login", app.UserHandler.Login)
-    }
-
-    protected := r.Group("/api/v1")
-    protected.Use(middleware.JWTAuthMiddleware(app.Config.Security.JWTSecret))
-    {
-        events := protected.Group("/events")
-        events.Use(middleware.AuthorizedRole([]string{"admin", "organizer"}))
-        {
-            events.POST("", app.EventHandler.Create)
-        }
-        
-        bookings := protected.Group("/bookings")
-        {
-            bookings.POST(":eventID", app.BookingHandler.Create)
-        }
     }
 }
---
+++ internal/router/router.go
@@ -0,0 +1,74 @@
+package router
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/anrisys/quicket/internal/validation"
+	"github.com/anrisys/quicket/pkg/di"
+	"github.com/anrisys/quicket/pkg/middleware"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func SetupRouter(app *di.App) *gin.Engine {
+	r := gin.New()
+
+	// Logging
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	// Custom validator
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		validation.RegisterCustomValidation(v)
+	}
+
+	// Middlewares
+	r.Use(middleware.ZerologLogger(), gin.Recovery(), middleware.ErrorHandler())
+
+	// Swagger
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// Routes
+	registerRoutes(r, app)
+
+	return r
+}
+
+func registerRoutes(r *gin.Engine, app *di.App) {
+	public := r.Group("/api/v1")
+	{
+		public.POST("/register", app.UserHandler.Register)
+		public.POST("/login", app.UserHandler.Login)
+	}
+
+	protected := r.Group("/api/v1")
+	protected.Use(middleware.JWTAuthMiddleware(app.Config.Security.JWTSecret))
+	{
+		events := protected.Group("/events")
+		events.Use(middleware.AuthorizedRole([]string{"admin", "organizer"}))
+		{
+			events.POST("", app.EventHandler.Create)
+		}
+
+		bookings := protected.Group("/bookings")
+		{
+			bookings.POST(":eventID", app.BookingHandler.Create)
+		}
+	}
+}
