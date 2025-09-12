3030931988d0cb7b4e993d17752b2373f748d600
Anris Y. Simorangkir <anris.y.simorangkir@gmail.com>
2025-09-12 11:00:56 +0700

    BOOKING-SERVICE: Add migration files

---
+++ booking-service/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+    `id`          BIGINT UNSIGNED PRIMARY KEY,
+    `public_id`   CHAR(36) NOT NULL UNIQUE,
+    `created_at`  DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at`  DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+    `deleted_at`  DATETIME(3) NULL,
+    INDEX `idx_users_deleted_at` (`deleted_at`)
+) ENGINE = InnoDB;
---
+++ booking-service/migrations/000002_create_events_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `events` (
+    `id`                BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `public_id`         CHAR(36) NOT NULL UNIQUE,
+    `title`             VARCHAR(256) NOT NULL,
+    `start_date`        DATETIME NOT NULL, 
+    `end_date`          DATETIME NOT NULL, 
+    `available_seats`   BIGINT UNSIGNED NOT NULL,
+    `created_at`        DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at`        DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+    `deleted_at`        DATETIME(3) NULL,
+    INDEX `idx_events_deleted_at` (`deleted_at`),
+    INDEX `idx_events_start_date` (`start_date`)
+) ENGINE = InnoDB;
